98ab32c7cf23f06046ad9e3289c5241179cbaab9
Zachary <taylor67469@yahoo.com>
2021-04-15 14:47:18 -0500

    did some fail functions for api

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
 // Dropdown functionality
 var dropdown1 = $('.dropdown-trigger1').dropdown();
-$('.dropdown-trigger2').dropdown();
-var stationNameFound;
-var trainFound;
+$('.dropdown-trigger2').dropdown(); 
+//variable for showing station name
+var stationNameFound; 
+
+
+//Click function to grab API INFO
 $(document).ready(function () {
   $('.Clickme').on('click', function (e) {
     var thetrain = e.target.id;
@@ -11,6 +14,7 @@ $(document).ready(function () {
         "api_key": "3125fa36e79d4814832e6a60e9f0a0a0",
         "LineCode": "BL",
       }
+      //Parameters for API
       $.ajax({
         url: "https://api.wmata.com/Rail.svc/json/jStations?" + $.param(params),
         type: "GET",
@@ -26,50 +30,60 @@ $(document).ready(function () {
               "api_key": "3125fa36e79d4814832e6a60e9f0a0a0",
               "Line": "BL",
             };
-      
+            //parameters for API
             $.ajax({
               url: "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/" + thetrain + '?' + $.param(params),
               type: "GET",
             })
               .then(function (data) {
-                trainFound = data.Trains.find(Trains => {
+                var trainFound = data.Trains.find(Trains => {
                   return Trains
                 })
                 populateList(stationNameFound,trainFound.Min);
               })
               .fail(function () {
-                alert("error");
+                populateFail(stationNameFound);
               })
           })
-          //run another ajax here
         })
         .fail(function () {
-          alert("error");
         })
     });
   })
 })
-
-//utility we built tofigure out what the boarding situation is
+//
+//utility we built to figure out what the boarding situation is
 function isArrivingOrBoarding(string) {
   if (string == "ARR") {
-    return "Arriving";
+    return "The train is  now arriving!";
   } else if (string == "BRD") {
-    return "Boarding";
+    return "The train is now boarding!";
   } else if (string == "") {
     return "Not Available";
   }
   else{
-    return "Train is arriving in "+string+" minutes.";
+    return "Train is arriving in "+string+" minutes!";
   }
 }
+//Populates HTML list
 function populateList(stationName,TrainMinutes){
   $('#data-entry-area').html(
     `
       <li class="li-styling"
-    color: var(--secondary-color)">Train Name: ${stationName}</li>
+    color: var(--secondary-color)">${stationName}</li>
       <li class="li-styling"
     color: var(--secondary-color)">${isArrivingOrBoarding(TrainMinutes)}</li>
+
+  `
+  );
+}
+function populateFail(stationName){
+  $('#data-entry-area').html(
+    `
+      <li class="li-styling"
+    color: var(--secondary-color)">${stationName}</li>
+      <li class="li-styling"
+    color: var(--secondary-color)">Train is not running.</li>
 
   `
   );
